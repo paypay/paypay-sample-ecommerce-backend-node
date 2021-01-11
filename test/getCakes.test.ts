d4d630e50f13efe9bb1d9a5724602e50394c8da9
solancer <t-srinivas.gowda@paypay-corp.co.jp>
2021-01-11 10:03:28 +0530

    Server stop fix

--- test/getCakes.test.ts
+++ test/getCakes.test.ts
@@ -1,8 +1,5 @@
-import * as cakes from '../src/cakes.json';
 import request from "supertest";
-import axios from "axios";
-import { App } from "../src/index";
-import { apiRouter } from "../src/routes";
+import { Server } from "../src";
 import payPayRestSDK from '@paypayopa/paypayopa-sdk-node';
 const conf = {
     clientId: '5345435fsdfsr54353454',
@@ -62,15 +59,17 @@ jest.mock('', () => {
 });
 
 describe('test list cakes', () => {
-    afterEach(() => jest.resetAllMocks());
+    afterEach(() => {
+        jest.resetAllMocks()
+    });
 
     it('list cakes', async () => {
         // const photos = await getPhotosByAlbumId(3);
         const mockHttpsCall = jest.fn();
-        const photos = await request(App).get('/cakes');
+        const photos = await request(Server).get('/cakes');
         expect(photos.body[0].id).toEqual(1);
-
         mockHttpsCall.mockClear();
+        Server.close()
     });
 
 
