579064af354b034240c79379223b71792977cfb2
ashraf-km <ashraf.km@robosoftin.com>
2021-01-13 10:45:51 +0530

    fix codacity issue

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,9 @@ app.disable("x-powered-by");
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/", apiRouter);
-const server = app.listen(port, () => { console.log(`server started at http://localhost:${port}`) });
+const server = app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}`);
+});
 export const Server = server;
 
 
